a896974c4a9647bc117690df253bd86153de7a10
elisiavalim <elisiavalim@gmail.com>
2022-09-29 17:46:54 -0300

    Add novos produtos

--- src/BaresRestaurantes/BaresRestaurantes/Models/Produto.cs
+++ src/BaresRestaurantes/BaresRestaurantes/Models/Produto.cs
@@ -12,8 +12,10 @@ namespace BaresRestaurantes.Models
         public List<Produto> GetProdutos()
         {
             var users = new List<Produto>();
-            users.Add(new Produto { Id = 1, Nome = "Filé a Parmegiana",Descricao= "Filé mignon à milanesa, gratinado com molho de tomate e queijo", Categoria ="Cantina Italiana"});
-            users.Add(new Produto { Id = 2, Nome = "Costela de Porco", Categoria="Costela de porco no barbecue com fritas"});
+            users.Add(new Produto { Id = 1, Nome = "Filé a Parmegiana",Descricao= "Filé mignon à milanesa, gratinado com molho de tomate e queijo.", Categoria = "Pratos Alacarte" });
+            users.Add(new Produto { Id = 2, Nome = "Costela de Porco", Descricao="Costela de porco no barbecue com fritas.", Categoria="Pratos Alacarte "});
+            users.Add(new Produto { Id = 3, Nome = "Moscow Mule", Descricao= "Vodka, espuma de gengbre,suco de limão, ginger Ale.", Categoria="Drinks"});
+            users.Add(new Produto { Id = 4, Nome = "Soda Italiana", Descricao= "Maçã verde, cranberry, limão, tangerina e morango.", Categoria="Drinks"});
 
             return users;
         }
