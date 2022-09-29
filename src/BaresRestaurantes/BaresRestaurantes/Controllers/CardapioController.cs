b0300385731cb87d460c0ba086f92f3616b88676
elisiavalim <elisiavalim@gmail.com>
2022-09-29 17:25:55 -0300

    Início do CRUD de produtos no cardápio.

---
+++ src/BaresRestaurantes/BaresRestaurantes/Controllers/CardapioController.cs
@@ -0,0 +1,20 @@
+﻿using BaresRestaurantes.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaresRestaurantes.Controllers
+{
+    [Authorize]
+    public class CardapioController : Controller
+    {
+        public IActionResult Index()
+        {
+            var produtos = new Produto().GetProdutos();
+            return View(produtos);
+        }
+    }
+}
