17aa7fded9e3f585b9c69388925bf78bd0380801
elisiavalim <elisiavalim@gmail.com>
2022-10-07 17:59:46 -0300

    Cria modelo Pedido e telas de listagem e edição de Status dos Pedidos

---
+++ src/BaresRestaurantes/BaresRestaurantes/Controllers/PedidoController.cs
@@ -0,0 +1,27 @@
+﻿using BaresRestaurantes.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace BaresRestaurantes.Controllers
+{
+    [Authorize]
+    public class PedidoController : Controller
+    {
+        public IActionResult Index()
+        {
+            var pedidos = new Pedido().GetPedidos();
+            return View(pedidos);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var pedidos = new Pedido().GetPedidos();
+
+            return View(pedidos.FirstOrDefault(prod => prod.Id == id));
+        }
+    }
+}
--- src/BaresRestaurantes/BaresRestaurantes/Models/Produto.cs
+++ src/BaresRestaurantes/BaresRestaurantes/Models/Produto.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace BaresRestaurantes.Models
 {
@@ -8,14 +9,16 @@ namespace BaresRestaurantes.Models
         public string Nome { get; set; }
         public string Descricao { get; set; }
         public string Categoria { get; set; }
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public float Preco { get; set; }
 
         public List<Produto> GetProdutos()
         {
             var users = new List<Produto>();
-            users.Add(new Produto { Id = 1, Nome = "Filé a Parmegiana",Descricao= "Filé mignon à milanesa, gratinado com molho de tomate e queijo.", Categoria = "Pratos Alacarte" });
-            users.Add(new Produto { Id = 2, Nome = "Costela de Porco", Descricao="Costela de porco no barbecue com fritas.", Categoria="Pratos Alacarte "});
-            users.Add(new Produto { Id = 3, Nome = "Moscow Mule", Descricao= "Vodka, espuma de gengbre,suco de limão, ginger Ale.", Categoria="Drinks"});
-            users.Add(new Produto { Id = 4, Nome = "Soda Italiana", Descricao= "Maçã verde, cranberry, limão, tangerina e morango.", Categoria="Drinks"});
+            users.Add(new Produto { Id = 1, Nome = "Filé a Parmegiana",Descricao= "Filé mignon à milanesa, gratinado com molho de tomate e queijo.", Categoria = "Pratos Alacarte", Preco = 99.90f });
+            users.Add(new Produto { Id = 2, Nome = "Costela de Porco", Descricao="Costela de porco no barbecue com fritas.", Categoria="Pratos Alacarte", Preco=79.90f});
+            users.Add(new Produto { Id = 3, Nome = "Moscow Mule", Descricao= "Vodka, espuma de gengbre,suco de limão, ginger Ale.", Categoria="Drinks", Preco = 17f });
+            users.Add(new Produto { Id = 4, Nome = "Soda Italiana", Descricao= "Maçã verde, cranberry, limão, tangerina e morango.", Categoria="Drinks", Preco = 8f });
 
             return users;
         }
