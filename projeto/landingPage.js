eea442475fbca96520b61d8886ed94be61e2f1a1
Samuel Oliveira <112700596+sammueloliveira@users.noreply.github.com>
2022-11-11 20:21:48 -0300

    Update landingPage.js

--- projeto/landingPage.js
+++ projeto/landingPage.js
@@ -1,1 +1,7 @@
+Vídeo de explicação da atividade está disponivel no site PucMinas: Etapa 3.
+
+
+ ## Samuel Oliveira = Landing Page
+ ## Landing Page, responsivo, integrando html, css e javaScript
+ 
 
