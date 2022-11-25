6855d3db10d6f3340f642f8079e5c7f5f108c924
Geison Amorim <79721330+Geison05@users.noreply.github.com>
2022-11-25 19:11:33 -0300

    Implementação da funcionlidade login

--- src/login/js/logar.js
+++ src/login/js/logar.js
@@ -1,18 +1,49 @@
+// Dados JSON
+var textoJSON = {
+    "cadastros": [
+        {
+            "usuario": "jubileu",
+            "nome": "Jubileu dos Anjos",
+            "email": "jubileu@abc.com.br",
+            "senha": "12345"
+        },
+        {
+            "usuario": "creonilde",
+            "nome": "Creonilde das Flores",
+            "email": "creonilde@abc.com.br",
+            "senha": "12345"
+        },
+        {
+            "usuario": "huliete",
+            "nome": "Huliete dos Campos",
+            "email": "huliete@abc.com.br",
+            "senha": "12345"
+        },
+        {
+            "usuario": "geriscreidon",
+            "nome": "Geriscreidon Silva",
+            "email": "geriscreidon@abc.com.br",
+            "senha": "12345"
+        }
+    ]
+};
+//Traformando JSON em objt
+var obj = JSON.parse(textoJSON);
+
 // entrando com os dados
-function logar(){
-    var email = document.getElementById("email");
+function logar() {
+    var useName = document.getElementById("usuario");
     var senha = document.getElementById("senha");
 
-    
-    // Verificação de  email/senha
-    if(email.value == "admin@admin" && senha.value == "admin"){
-        localStorage.setItem("acesso", true);    
+
+    // Autenticação de Usuário/Senha
+    if (useName.value == obj.usuario && senha.value == obj.senha) {
+        localStorage.setItem("acesso", true);
         alert("Usuario autenticado!")
 
         window.location.href = "../index.html";
-    
-    }else{
+
+    } else {
         alert("Usuario ou senha invalidos!");
     }
-}
-
+};
