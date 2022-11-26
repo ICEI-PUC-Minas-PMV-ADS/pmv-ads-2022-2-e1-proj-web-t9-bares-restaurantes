fcb976f3f12f57d6458ce610c2b186215b75dec4
Samuel Oliveira <112700596+sammueloliveira@users.noreply.github.com>
2022-11-26 15:14:29 -0300

    Delete projeto directory

--- projeto/landingPage.js
+++
@@ -1,7 +0,0 @@
-Vídeo de explicação da atividade está disponivel no site PucMinas: Etapa 3.
-
-
- ## Samuel Oliveira = Landing Page
- ## Landing Page, responsivo, integrando html, css e javaScript
- 
-
